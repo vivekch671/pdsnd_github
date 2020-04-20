1b8be1fd5976585ea85e31adba13234b594bae3a
Vivek <vivekc671@gmail.com>
2020-04-20 16:26:29 +0530

    Docstring for load_city - file used
    Docstring for load_city is updated to specify that respective city data
    file is to be used

--- bikeshare.py
+++ bikeshare.py
@@ -45,7 +45,7 @@ def get_day():
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
-
+    Data pulled from respective city file
     Args:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
